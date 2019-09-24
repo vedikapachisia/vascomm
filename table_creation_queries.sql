c581662fdcb71367311419fdbfaaa716fc5d7bcb
Shayak Banerjee <shayak.banerjee.13@gmail.com>
2019-09-24 13:39:12 +0530

    pretty printing of table creation queries

--- table_creation_queries.sql
+++ table_creation_queries.sql
@@ -7,41 +7,41 @@ CREATE TABLE plans(
     sms_rate DECIMAL,
     data_rate DECIMAL,
     price DECIMAL
-    );
+);
+
 CREATE TABLE user_details(
     user_id INT AUTO_INCREMENT,
     user_name VARCHAR(45),
     dob DATE,
     phone INT,
     plan_id VARCHAR(45)
-    );
+);
+
 CREATE TABLE usage_details(
     user_id INT AUTO_INCREMENT,
     call_amount DECIMAL,
     sms_amount DECIMAL,
     data_amount DECIMAL,
     total_amount DECIMAL
-    );
+);
+
 CREATE TABLE calls_log(
     call_id INT AUTO_INCREMENT,
     user_id INT AUTO_INCREMENT,
     call_start TIME,
     call_end TIME
-    );
+);
+
 CREATE TABLE sms_log(
     sms_id INT AUTO_INCREMENT,
     user_id INT AUTO_INCREMENT, 
     message_time TIME
-    );
+);
+
 CREATE TABLE data_log(
     data_id INT AUTO_INCREMENT,
     user_id INT AUTO_INCREMENT,
     usage_time TIME,
     data_used DECIMAL
-    );
-    
-    
-    
-    
-    
+);
     
