12e20ba740ac0ee151ab7206961b5d87e1b88066
Shayak Banerjee <shayak.banerjee.13@gmail.com>
2019-10-01 03:58:58 +0530

    Added all tables to the database
    populated plans
    populated user_details

--- djangoproject/webapp/models.py
+++ djangoproject/webapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Plans(models.Model):
+    plan_id = models.CharField(max_length = 6, primary_key=True)
+    free_talktime = models.IntegerField()
+    free_sms = models.IntegerField()
+    free_data = models.IntegerField()
+    call_rate = models.IntegerField()
+    sms_rate = models.DecimalField(decimal_places=2, max_digits=30)
+    data_rate = models.DecimalField(decimal_places=2, max_digits=30)
+    price = models.DecimalField(decimal_places=2, max_digits=30)
+
+class UserDetails(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=45)
+    dob = models.DateField()
+    phone = models.CharField(max_length=10)
+    plan_id = models.ForeignKey(Plans, on_delete=models.CASCADE, default="INT600")
+
+class UsageDetails(models.Model):
+    user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    call_amount = models.DecimalField(decimal_places=2, max_digits=30)
+    sms_amount = models.DecimalField(decimal_places=2, max_digits=30)
+    data_amount = models.DecimalField(decimal_places=2, max_digits=30)
+    total_amount = models.DecimalField(decimal_places=2, max_digits=30)
+
+class CallsLog(models.Model):
+    call_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    call_start = models.TimeField()
+    call_end = models.TimeField()
+
+class SmsLog(models.Model):
+    sms_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    message_time = models.TimeField()
+
+class DataLog(models.Model):
+    data_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    usage_time = models.TimeField()
+    data_used = models.DecimalField(decimal_places=2, max_digits=30)
--- djangoproject/webapp/views.py
+++ djangoproject/webapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
+from .models import *
+
 # Create your views here.
 def index(request):
+    obj = Plans(plan_id='INT600', free_talktime=200, free_sms=1000, free_data=12, call_rate=5, sms_rate=3, data_rate=1, price=600)
+    obj.save()
     return render(request, 'index.html')
 
 def text(request):
