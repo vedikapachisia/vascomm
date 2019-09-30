88d1318917f7003701c7494139bc2694b4187d4b
Shayak Banerjee <shayak.banerjee.13@gmail.com>
2019-09-30 20:51:00 +0530

    Update settings.py
    Added 'webapp' to the list of directories that will be looked up for
    templates.

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['webapp'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
